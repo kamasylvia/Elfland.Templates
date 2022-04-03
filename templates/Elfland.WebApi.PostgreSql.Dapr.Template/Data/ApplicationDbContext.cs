1e1f62f58e9e33a474a2c0ce09004cb5277f6831
Yue Zhang <zhangyue.dev@outlook.com>
2022-04-03 21:07:38 +0800

    Add gitignore

---
+++ templates/Elfland.WebApi.PostgreSql.Dapr.Template/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Elfland.WebApi.PostgreSql.Dapr.Template.Data;
+
+public class ApplicationDbContext: DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public ApplicationDbContext(DbContextOptions options, IConfiguration configuration)
+        : base(options)
+    {
+        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("PostgreSQL"));
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
+
---
+++ templates/Elfland.WebApi.PostgreSql.Dapr.Template/Data/Initializers/ApplicationDbContextInitializer.cs
@@ -0,0 +1,14 @@
+﻿namespace Elfland.WebApi.PostgreSql.Dapr.Template.Data.Initializers;
+
+public static class ApplicationDbContextInitializer
+{
+    public static async Task InitializeDbContextAsync(
+        this IServiceProvider serviceProvider,
+        IConfiguration configuration,
+        IWebHostEnvironment environment)
+    {
+        using var serviceScope = serviceProvider.CreateScope();
+        var context = serviceScope?.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    }
+}
+
